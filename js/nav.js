e339b2771f376c7cc01a794d75bba15bcbb6c92f
Liam Callanan <liam_callanan@brown.edu>
2017-07-06 18:23:52 -0700

    another site redesign

--- js/nav.js
+++ js/nav.js
@@ -1,36 +1,57 @@
 
 (function(){
-	"use strict";
+	document.$class = document.getElementsByClassName;
+
+	let scrolledDown = false;
 
 	function nav() {
-		var isOpen = false;
-		document.getElementsByClassName("menu__toggle")[0].addEventListener("click", function() {
-			var 
-				wrapper = document.getElementsByClassName("transform__wrapper")[0],
-				header = document.getElementsByClassName("header")[0];
+		let isOpen = false;
+		document.$class('nav__toggle')[0].addEventListener('click', function() {
+			let wrapper = document.$class('transform__wrapper')[0],
+				header = document.$class('header')[0];
 
 			isOpen = !isOpen;
 
 			if (isOpen) {
-				wrapper.classList.add("transform--left");
-				header.classList.add("transform--left");
+				wrapper.classList.add('transform--left');
+				header.classList.add('transform--left');
 				return;
 			}
 
-			wrapper.classList.remove("transform--left");
-			header.classList.remove("transform--left");
+			wrapper.classList.remove('transform--left');
+			header.classList.remove('transform--left');
 		});
 	}
 
 	function scroll() {
-		var header = document.getElementsByClassName("header")[0];
+		if (window.scrollY > 50 && !scrolledDown) {
+			let header = document.$class('header')[0],
+				links = document.$class('nav__link'),
+				len = links.length,
+				i;
 
-		if (window.scrollY > 50) {
-			header.classList.add("header--solid");
+			header.classList.add('header--solid');
+			for (i = 0; i < len; i++) {
+				links[i].classList.add('nav__link--solid');
+			}
+
+			scrolledDown = true;
 			return;	
 		}
 
-		header.classList.remove("header--solid");
+		if (window.scrollY <= 50 && scrolledDown) {
+			let header = document.$class('header')[0],
+				links = document.$class('nav__link'),
+				len = links.length,
+				i;
+
+			header.classList.remove('header--solid');
+			for (i = 0; i < len; i++) {
+				links[i].classList.remove('nav__link--solid');
+			}
+
+			scrolledDown = false;
+		}
 	}
 
 	window.onload = function() {
